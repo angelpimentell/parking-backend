9b577061ea5814e4b0db764da959f361376c7cd5
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-03-11 19:51:33 -0400

    Add tests

---
+++ tests/Feature/Models/Parking/TicketTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Models\Parking;
+
+use Tests\TestCase;
+
+class TicketTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
---
+++ tests/Feature/Models/System/SettingTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Models\System;
+
+use Tests\TestCase;
+
+class SettingTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
