18c834db256dcf4132c497138d819a2d00fbe2d4
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-03-11 19:51:20 -0400

    Add factories

---
+++ database/factories/Parking/TicketPlanFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\Parking;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Parking\TicketPlan>
+ */
+class TicketPlanFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
