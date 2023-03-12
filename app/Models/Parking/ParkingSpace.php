b567a5cc8cb6778283442aa0331740f4533981b1
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-03-12 19:07:16 -0400

    Add parking space relations

--- app/Models/Parking/ParkingSpace.php
+++ app/Models/Parking/ParkingSpace.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class ParkingSpace extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the payment associated with the ticker.
+     */
+    public function parkingLevel(): HasOne
+    {
+        return $this->hasOne(ParkingLevel::class);
+    }
+
+    /**
+     * Get the tickets that owns the parking space.
+     */
+    public function tickets(): BelongsTo
+    {
+        return $this->belongsTo(Ticket::class);
+    }
 }
