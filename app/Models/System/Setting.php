3deb8849412c589ed9638122c0e98c999a849bf7
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-03-12 19:23:07 -0400

    Add setting relations

--- app/Models/System/Setting.php
+++ app/Models/System/Setting.php
@@ -2,10 +2,19 @@
 
 namespace App\Models\System;
 
+use App\Models\Parking\Payment;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the user that owns the setting.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(Payment::class);
+    }
 }
