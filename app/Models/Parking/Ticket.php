7187141cd0d91fb7a5a70efd6184d604fbb719ac
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-03-12 19:07:47 -0400

    Add ticket relations

--- app/Models/Parking/Ticket.php
+++ app/Models/Parking/Ticket.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the payment associated with the ticker.
+     */
+    public function ticketPlan(): HasOne
+    {
+        return $this->hasOne(TicketPlan::class);
+    }
+
+    /**
+     * Get the parking space associated with the ticker.
+     */
+    public function parkingSpace(): HasOne
+    {
+        return $this->hasOne(ParkingSpace::class);
+    }
+
+    /**
+     * Get the payment that owns the ticker.
+     */
+    public function payment(): BelongsTo
+    {
+        return $this->belongsTo(Payment::class);
+    }
+
+    /**
+     * Get the parking level.
+     */
+    public function parkingLevel(): HasOneThrough
+    {
+        return $this->hasOneThrough(ParkingSpace::class, ParkingLevel::class);
+    }
+
 }
