d5f45db3b591f2bde8cc88c95e6f9852c9e9590c
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-03-12 11:50:23 -0400

    Add Ticket migration

--- database/migrations/2023_03_11_234547_create_tickets_table.php
+++ database/migrations/2023_03_11_234547_create_tickets_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Parking\ParkingSpace;
+use App\Models\Parking\TicketPlan;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(TicketPlan::class);
+            $table->foreignIdFor(ParkingSpace::class);
+            $table->string('name');
+            $table->boolean('active');
+            $table->date('expiration_date');
             $table->timestamps();
         });
     }
