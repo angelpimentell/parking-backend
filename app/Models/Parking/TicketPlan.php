e080e41ac9f8232eeff3d4459c87b080831e2b66
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-03-12 19:39:16 -0400

    Add fillable attribute in TicketPlan model

--- app/Models/Parking/TicketPlan.php
+++ app/Models/Parking/TicketPlan.php
@@ -10,6 +10,19 @@ class TicketPlan extends Model
     use HasFactory;
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'price',
+        'hours',
+        'penalty_per_hour',
+        'description',
+    ];
+
+
+    /**
      * Get the tickets that owns ticket plan.
      */
     public function tickets(): BelongsTo
