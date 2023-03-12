8ec3d289bce866d1621dd67eebf05a67055993c9
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-03-12 11:50:29 -0400

    Add Payment migration

--- database/migrations/2023_03_11_234602_create_payments_table.php
+++ database/migrations/2023_03_11_234602_create_payments_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Parking\Payment;
+use App\Models\System\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Payment::class);
+            $table->foreignIdFor(User::class, 'cashier_user_id');
+            $table->decimal('amount',9, 4);
+            $table->decimal('amount_paid',9, 4);
+            $table->decimal('penalty_amount',9, 4);
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
