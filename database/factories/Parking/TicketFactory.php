d644bfd9de9d1826f098016a6144efdd16b9aaea
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-03-13 19:38:21 -0400

    Add definition for TicketFactory

--- database/factories/Parking/TicketFactory.php
+++ database/factories/Parking/TicketFactory.php
@@ -4,6 +4,9 @@ namespace Database\Factories\Parking;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+use App\Models\Parking\ParkingSpace;
+use App\Models\Parking\TicketPlan;
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Parking\Ticket>
  */
@@ -17,7 +20,11 @@ class TicketFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'ticket_plan_id' => TicketPlan::factory(),
+            'parking_space_id' => ParkingSpace::factory(),
+            'name' => fake()->name(),
+            'active' => fake()->boolean(),
+            'expiration_date' => fake()->dateTime(),
         ];
     }
 }
