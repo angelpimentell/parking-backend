fd8bd39683831f86f592b955285f141666636074
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-03-15 14:49:22 -0400

    Change logic for ParkingLevelController.php

--- app/Http/Controllers/Parking/ParkingLevelController.php
+++ app/Http/Controllers/Parking/ParkingLevelController.php
@@ -2,49 +2,16 @@
 
 namespace App\Http\Controllers\Parking;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\CrudController;
 use App\Models\Parking\ParkingLevel;
-use Illuminate\Http\Request;
 
-class ParkingLevelController extends Controller
+
+class ParkingLevelController extends CrudController
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        return ParkingLevel::all();
-    }
 
     /**
-     * Store a newly created resource in storage.
+     * Respective model for controller
      */
-    public function store(Request $request)
-    {
-        return ParkingLevel::create($request->all());
-    }
+    protected $model = ParkingLevel::class;
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        return ParkingLevel::find($id);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        return ParkingLevel::find($id)->update($request->all());
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        return ParkingLevel::find($id)->delete();
-    }
 }
