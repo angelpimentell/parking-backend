2d89be14e88c16c75c93ef16b407de37f0a522a2
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-03-17 19:01:47 -0400

    Change payment migration with new foreign keys

--- database/migrations/2023_03_11_234602_create_payments_table.php
+++ database/migrations/2023_03_11_234602_create_payments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-use App\Models\Parking\Payment;
+use App\Models\Parking\Ticket;
 use App\Models\System\User;
 
 return new class extends Migration {
@@ -15,8 +15,8 @@ return new class extends Migration {
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Payment::class);
-            $table->foreignIdFor(User::class, 'cashier_user_id');
+            $table->foreignIdFor(Ticket::class);
+            $table->foreignIdFor(User::class);
             $table->decimal('amount_paid', 9, 4);
             $table->decimal('penalty_amount', 9, 4);
             $table->string('description')->nullable();
