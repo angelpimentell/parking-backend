a76c4756407b71121caadf31ab7147023badf36d
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-06-06 22:01:09 -0400

    Add constants
    Constants:
    - default_pagination
    - max_pagination
    - filter_query

--- config/constants.php
+++ config/constants.php
@@ -2,4 +2,12 @@
 
 return [
     'datetime_format' => 'Y-m-d H:m:s',
+    'default_pagination' => 25,
+    'max_pagination' => 100,
+
+    'filter_query' => [
+        'name' => 'filter_columns',
+        'trueValue' => '1',
+        'falseValue' => '0',
+    ]
 ];
