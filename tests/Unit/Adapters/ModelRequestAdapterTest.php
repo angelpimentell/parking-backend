4c6755ece8a983eb4d64e43f625f5a41b5a93441
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-06-07 17:39:00 -0400

    Add ModelRequestAdapter.php

---
+++ tests/Unit/Adapters/ModelRequestAdapterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Adapters;
+
+use Tests\TestCase;
+
+class ModelRequestAdapterTest  extends TestCase
+{
+    public function test_everything_works()
+    {
+        $this->assertTrue(false);
+    }
+}
